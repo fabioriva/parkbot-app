f2603d8b7153842eaf537d2aae85d933ced9db46
Fabio Riva <f.riva@sotefin.ch>
2025-08-20 08:16:11 +0200

    comments deleted

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,8 +1,5 @@
 import { MongoClient } from "mongodb";
 
-console.log(import.meta.env.VITE_MONGODB_URI);
-console.log(import.meta.env.VITE_MONGODB_DB);
-
 const client = new MongoClient(import.meta.env.VITE_MONGODB_URI);
 
 export const db = client.db(import.meta.env.VITE_MONGODB_DB);
