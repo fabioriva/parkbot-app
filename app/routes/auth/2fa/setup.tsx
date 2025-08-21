0132ed3a10420e95f137d36f9e9c02a8152de748
Fabio Riva <f.riva@sotefin.ch>
2025-08-21 17:55:11 +0200

    use @oslo/otp for OTP code generation

--- app/routes/auth/2fa/setup.tsx
+++ app/routes/auth/2fa/setup.tsx
@@ -1,5 +1,8 @@
+import { encodeBase64 } from "@oslojs/encoding";
+import { createTOTPKeyURI } from "@oslojs/otp";
 import { useTranslation } from "react-i18next";
 import { Form, Link, redirect } from "react-router";
+import { renderSVG } from "uqr";
 import Button from "~/components/submitFormButton";
 import {
   Card,
@@ -9,6 +12,23 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+
+export async function loader({ context, request }: Route.LoaderArgs) {
+  // const { session, user } = await getSession(request);
+  // await getSession(request);
+  const totpKey = new Uint8Array(20);
+  crypto.getRandomValues(totpKey);
+  const encodedTOTPKey = encodeBase64(totpKey);
+  const keyURI = createTOTPKeyURI(
+    "ParkbotApp",
+    "user.username",
+    totpKey,
+    30,
+    6
+  );
+  const qrcode = renderSVG(keyURI);
+  return qrcode;
+}
 
 export default function TwoFASetup({ loaderData }: Route.ComponentProps) {
   let { t } = useTranslation();
