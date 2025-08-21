a969c292edb352180dc517944fe7d893b7389152
Fabio Riva <f.riva@sotefin.ch>
2025-08-21 16:55:32 +0200

    2fa pages first commit

---
+++ app/routes/auth/2fa/reset.tsx
@@ -0,0 +1,7 @@
+export default function TwoFAReset({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      <h1>2FA reset route</h1>
+    </div>
+  );
+}
---
+++ app/routes/auth/2fa/setup.tsx
@@ -0,0 +1,7 @@
+export default function TwoFASetup({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      <h1>2FA setup route</h1>
+    </div>
+  );
+}
