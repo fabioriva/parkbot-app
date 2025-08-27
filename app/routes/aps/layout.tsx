840e3ca1ca24a080e0462b046bec1743577b879a
Fabio Riva <f.riva@sotefin.ch>
2025-08-27 14:49:09 +0200

    added logout link

--- app/routes/aps/layout.tsx
+++ app/routes/aps/layout.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function Aps() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Outlet />
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Outlet />
+        <Link className="underline underline-offset-4" to="/logout">
+          Logout
+        </Link>
+      </div>
     </div>
   );
 }
