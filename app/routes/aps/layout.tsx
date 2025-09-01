d9c8d61f23d30c40fc98927776ca90688702c2aa
Fabio Riva <f.riva@sotefin.ch>
2025-09-01 15:13:38 +0200

    aps layout initial commit

--- app/routes/aps/layout.tsx
+++ app/routes/aps/layout.tsx
@@ -1,14 +1,35 @@
 import { Link, Outlet } from "react-router";
+import { AppSidebar } from "~/components/app-sidebar";
+import { Separator } from "~/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/ui/sidebar";
 
-export default function Aps() {
+export default function ApsLayout() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Outlet />
-        <Link className="underline underline-offset-4" to="/logout">
-          Logout
-        </Link>
-      </div>
-    </div>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "19rem",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <span>...</span>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
