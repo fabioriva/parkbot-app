db92cc0cdcc3ce95d5efa7b666715a938dc75a11
Fabio Riva <f.riva@sotefin.ch>
2025-09-05 11:56:14 +0200

    fix: app-sidebar component typecheck error

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { GalleryVerticalEnd } from "lucide-react";
+import { CircleParking } from "lucide-react";
 
 import {
   Sidebar,
@@ -14,144 +14,23 @@ import {
   SidebarMenuSubItem,
 } from "~/components/ui/sidebar";
 
-// This is sample data.
 const data = {
   navMain: [
-    // {
-    //   title: "Getting Started",
-    //   url: "#",
-    //   items: [
-    //     {
-    //       title: "Installation",
-    //       url: "#",
-    //     },
-    //     {
-    //       title: "Project Structure",
-    //       url: "#",
-    //     },
-    //   ],
-    // },
-    //   {
-    //     title: "Building Your Application",
-    //     url: "#",
-    //     items: [
-    //       {
-    //         title: "Routing",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Data Fetching",
-    //         url: "#",
-    //         isActive: true,
-    //       },
-    //       {
-    //         title: "Rendering",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Caching",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Styling",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Optimizing",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Configuring",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Testing",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Authentication",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Deploying",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Upgrading",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Examples",
-    //         url: "#",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     title: "API Reference",
-    //     url: "#",
-    //     items: [
-    //       {
-    //         title: "Components",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "File Conventions",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Functions",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "next.config.js Options",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "CLI",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Edge Runtime",
-    //         url: "#",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     title: "Architecture",
-    //     url: "#",
-    //     items: [
-    //       {
-    //         title: "Accessibility",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Fast Refresh",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Next.js Compiler",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Supported Browsers",
-    //         url: "#",
-    //       },
-    //       {
-    //         title: "Turbopack",
-    //         url: "#",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     title: "Community",
-    //     url: "#",
-    //     items: [
-    //       {
-    //         title: "Contribution Guide",
-    //         url: "#",
-    //       },
-    //     ],
-    //   },
+    {
+      title: "Getting Started",
+      url: "#",
+      items: [
+        {
+          title: "Dashboard",
+          url: "#",
+          isActive: true,
+        },
+        {
+          title: "Operations",
+          url: "#",
+        },
+      ],
+    },
   ],
 };
 
@@ -164,11 +43,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <GalleryVerticalEnd className="size-4" />
+                  <CircleParking className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-medium">Parkbot web service</span>
-                  <span className="">v1.0.0</span>
+                  <span className="text-xs">v1.0.0</span>
                 </div>
               </a>
             </SidebarMenuButton>
@@ -189,7 +68,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
                     {item.items.map((item) => (
                       <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                        <SidebarMenuSubButton asChild isActive={item?.isActive}>
                           <a href={item.url}>{item.title}</a>
                         </SidebarMenuSubButton>
                       </SidebarMenuSubItem>
