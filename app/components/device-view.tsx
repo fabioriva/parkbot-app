600ccca031711f7e54542171f21facb8dbd177bf
Fabio Riva <f.riva@sotefin.ch>
2025-09-30 12:17:52 +0200

    device-view code refactoring

--- app/components/device-view.tsx
+++ app/components/device-view.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import {
   Accordion,
   AccordionContent,
@@ -12,7 +13,8 @@ interface DeviceProps {
   device: object;
 }
 
-function Info({ color, label, status }) {
+function Info({ bit, color }) {
+  const { label, status } = bit;
   return (
     <Badge
       className={
@@ -28,17 +30,94 @@ function Info({ color, label, status }) {
 }
 
 export function DeviceView({ device }: DeviceProps) {
-  console.log(device.views);
-
+  console.log(typeof device.views, device.views.length, device.views);
+  const statusWord = [
+    ...(device.views[0].status >>> 0).toString(2).padEnd(16, "0"),
+  ].map((b, key) => ({
+    b,
+  }));
+  console.log(statusWord);
   return (
-    <Tabs defaultValue="tab-1">
+    <Tabs defaultValue="tab-0">
       <TabsList className="w-full">
-        <TabsTrigger value="tab-1">Main</TabsTrigger>
-        <TabsTrigger value="tab-2">Garage</TabsTrigger>
-        <TabsTrigger value="tab-3">Silomat</TabsTrigger>
+        {device.views.map((item, key) => (
+          <TabsTrigger value={`tab-${key}`} key={key}>
+            {item.name}
+          </TabsTrigger>
+        ))}
       </TabsList>
-      <TabsContent value="tab-1">
-        {/* <DeviceInfo /> */}
+      {device.views.map((view, key) => (
+        <TabsContent value={`tab-${key}`} key={key}>
+          <Accordion
+            type="multiple" // "single"
+            collapsible
+            className="w-full"
+            // defaultValue="item-0"
+          >
+            {view.drives.map((drive, key) => (
+              <AccordionItem value={`item-${key}`}>
+                <AccordionTrigger className="flex hover:no-underline">
+                  <div
+                    className={clsx("flex gap-3 grow", {
+                      "text-green-600": drive.enable,
+                    })}
+                  >
+                    {drive.name}
+                  </div>
+                  <div>{drive.speed}&nbsp;Hz</div>
+                  <div>{drive.current}&nbsp;A</div>
+                  {/* <Info bit={drive.enable} color="green" /> */}
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-1.5">
+                  <div className="flex justify-between text-muted-foreground">
+                    <span>0</span>
+                    <span>status word</span>
+                    <span>15</span>
+                  </div>
+                  <div className="grid grid-cols-16 gap-0.5">
+                    {[...(drive.status >>> 0).toString(2).padEnd(16, "0")].map(
+                      (bit, key) => (
+                        <p
+                          className={clsx("rounded-xs text-center", {
+                            "bg-green-600": Boolean(parseInt(bit)),
+                            "bg-slate-200": !Boolean(parseInt(bit)),
+                          })}
+                        >
+                          {bit}
+                        </p>
+                      )
+                    )}
+                  </div>
+                  <div className="grid grid-cols-4 gap-3">
+                    <div className="flex flex-col">
+                      <span className="text-muted-foreground">{"Speed"} </span>
+                      <span>{drive.speed}&nbsp;Hz</span>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-muted-foreground">
+                        {"Current"}{" "}
+                      </span>
+                      <span>{drive.current}&nbsp;A</span>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-muted-foreground">{"Load"} </span>
+                      <span>{drive.load}&nbsp;%</span>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-muted-foreground">
+                        {"Last trip"}{" "}
+                      </span>
+                      <span>{drive.trip}</span>
+                    </div>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </TabsContent>
+      ))}
+      {/* <TabsContent value="tab-1">
+        <DeviceInfo />
         <Accordion className="border px-3 w-full" type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger className="flex hover:no-underline">
@@ -72,7 +151,7 @@ export function DeviceView({ device }: DeviceProps) {
       </TabsContent>
       <TabsContent value="tab-3">
         <DeviceInfo />
-      </TabsContent>
+      </TabsContent> */}
     </Tabs>
   );
 }
