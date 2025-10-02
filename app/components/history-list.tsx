0e1baf36198b742f5457269d43728509fccd355e
Fabio Riva <f.riva@sotefin.ch>
2025-10-02 14:24:31 +0200

    avatar icon code refactoring

--- app/components/history-list.tsx
+++ app/components/history-list.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import {
   ArrowLeftRight,
   ArrowRightLeft,
@@ -23,98 +24,31 @@ interface HistoryListProps {
 }
 
 const Icon = ({ id }) => (
-  <>
-    {id === 1 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-red-200 rounded-full text-red-600"
-        }
-      >
-        <BellOff size={24} />
-      </div>
+  <div
+    className={clsx(
+      "flex items-center justify-center h-[36px] w-[36px] border rounded-full",
+      {
+        "bg-red-200 border-red-600 text-red-600": id === 1,
+        "bg-green-200 border-green-600 text-green-600": id === 2,
+        "bg-yellow-200 border-yellow-600 text-yellow-600": id === 3 || id === 4,
+        "bg-blue-200 border-blue-600 text-blue-600": id === 5 || id === 7,
+        "bg-purple-200 border-purple-600 text-purple-600": id === 6 || id === 8,
+        "bg-amber-200 border-amber-600 text-amber-600": id >= 9 && id <= 14,
+        "bg-slate-200 border-slate-600 text-slate-600": id > 14,
+      }
     )}
-    {id === 2 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-green-200 rounded-full text-green-600"
-        }
-      >
-        <Bell size={24} />
-      </div>
-    )}
-    {id === 3 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-yellow-200 rounded-full text-yellow-600"
-        }
-      >
-        <UserRoundCog size={24} />
-      </div>
-    )}
-    {id === 4 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-yellow-200 rounded-full text-yellow-600"
-        }
-      >
-        <Bell size={24} />
-      </div>
-    )}
-    {id === 5 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-blue-200 rounded-full text-blue-600"
-        }
-      >
-        <UserRoundPlus size={24} />
-      </div>
-    )}
-    {id === 6 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-purple-200 rounded-full text-purple-600"
-        }
-      >
-        <UserRoundMinus size={24} />
-      </div>
-    )}
-    {id === 7 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-blue-200 rounded-full text-blue-600"
-        }
-      >
-        <ArrowLeftRight size={24} />
-      </div>
-    )}
-    {id === 8 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-purple-200 rounded-full text-purple-600"
-        }
-      >
-        <ArrowRightLeft size={24} />
-      </div>
-    )}
-    {id >= 9 && id <= 14 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-amber-200 rounded-full text-amber-600"
-        }
-      >
-        <Bookmark size={24} />
-      </div>
-    )}
-    {id > 14 && (
-      <div
-        className={
-          "flex items-center justify-center p-2 bg-slate-200 rounded-full text-slate-600"
-        }
-      >
-        <X size={24} />
-      </div>
-    )}
-  </>
+  >
+    {id === 1 && <BellOff size={20} />}
+    {id === 2 && <Bell size={20} />}
+    {id === 3 && <UserRoundCog size={20} />}
+    {id === 4 && <Bell size={20} />}
+    {id === 5 && <UserRoundPlus size={20} />}
+    {id === 6 && <UserRoundMinus size={20} />}
+    {id === 7 && <ArrowLeftRight size={20} />}
+    {id === 8 && <ArrowRightLeft size={20} />}
+    {id >= 9 && id <= 14 && <Bookmark size={20} />}
+    {id > 14 && <X size={20} />}
+  </div>
 );
 
 export function HistoryList({ history }: HistoryListProps) {
@@ -124,7 +58,7 @@ export function HistoryList({ history }: HistoryListProps) {
         <div className="flex items-center justify-between text-sm" key={key}>
           <div className="flex items-center gap-4">
             <Icon id={item.operation.id} />
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-0.5">
               <p className="leading-none font-medium">
                 {item.device.key} - {item.mode.key}
               </p>
@@ -134,8 +68,8 @@ export function HistoryList({ history }: HistoryListProps) {
             </div>
           </div>
           <div className="flex flex-col text-right text-muted-foreground text-sm">
-            <p>{item.date.slice(0, 10)}</p>
-            <p>{item.date.slice(11, 19)}</p>
+            <span>{item.date.slice(0, 10)}</span>
+            <span>{item.date.slice(11, 19)}</span>
           </div>
         </div>
       ))}
