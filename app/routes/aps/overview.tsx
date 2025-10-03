6346422e5dcff97c168dd6a6dcd3a8ed287992e1
Fabio Riva <f.riva@sotefin.ch>
2025-10-03 11:30:46 +0200

    overview layout, set grid cols number based on devices length

--- app/routes/aps/overview.tsx
+++ app/routes/aps/overview.tsx
@@ -17,7 +17,9 @@ export default function Overview({ loaderData }: Route.ComponentProps) {
   const url = `${import.meta.env.VITE_WEBSOCK_URL}/${loaderData?.aps}/overview`;
   const { data } = useData(url, { initialData: loaderData?.data });
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 items-start">
+    <div
+      className={`grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-${data.devices[0].length} gap-4 items-start>`}
+    >
       {data.devices.flat(1).map((item, key) => (
         <Device device={item} enhanced={true} key={key} />
       ))}
