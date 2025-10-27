16eb46bde740243be2f8ecd0ea958c22c6955042
Fabio Riva <f.riva@sotefin.ch>
2025-10-27 11:21:13 +0100

    navigate back to previous page

--- app/routes/not-found.tsx
+++ app/routes/not-found.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from "react-i18next";
-import { data, href, Link } from "react-router";
+import { data, Link, useNavigate } from "react-router";
 
 export async function loader() {
   return data(null, { status: 404 });
 }
 
 export default function Component() {
+  const navigate = useNavigate();
   let { t } = useTranslation("notFound");
 
   return (
@@ -13,7 +14,13 @@ export default function Component() {
       <div className="flex flex-col gap-6 text-center">
         <h1 className="text-2xl font-bold">{t("title")}</h1>
         <p className="text-sm">{t("description")}</p>
-        <Link className="underline" to={href("/")}>
+        <Link
+          className="underline"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+        >
           {t("backToHome")}
         </Link>
       </div>
