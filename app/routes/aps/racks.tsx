75acb51dfa812b8bf5e109e6cb9049664570c429
Fabio Riva <f.riva@sotefin.ch>
2025-10-30 17:05:11 +0100

    Racks page first commit

---
+++ app/routes/aps/racks.tsx
@@ -0,0 +1,53 @@
+import { ChevronRightIcon, CircleCheck } from "lucide-react";
+import { Error } from "~/components/error";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "~/components/ui/item";
+import fetcher from "~/lib/fetch.server";
+
+import type { Route } from "./+types/racks";
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const url = `${import.meta.env.VITE_BACKEND_URL}/${params?.aps}/racks`;
+  const data = await fetcher(url);
+  return { data };
+}
+
+export default function Racks({ loaderData, params }: Route.ComponentProps) {
+  if (!loaderData?.data) return <Error />;
+  console.log(loaderData?.data);
+  const data = loaderData?.data;
+  return (
+    <div className="flex flex-wrap gap-3">
+      {data.map((item) => (
+        <Item
+          className="min-w-xs"
+          variant="outline"
+          key={item.deviceNr}
+          asChild
+        >
+          <a
+            href={`/${params?.aps}/rack/${item.rack.nr}?name=${item.deviceName}&nr=${item.deviceNr}`}
+          >
+            <ItemContent>
+              <ItemTitle>{item.deviceName}</ItemTitle>
+              <ItemDescription>
+                {item.type} {item.key}
+              </ItemDescription>
+            </ItemContent>
+            <ItemActions
+              className={item.online.status ? "text-ready" : "text-alert"}
+            >
+              <CircleCheck />
+            </ItemActions>
+          </a>
+        </Item>
+      ))}
+    </div>
+  );
+}
