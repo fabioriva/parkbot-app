36e4d8382c63cefdaac5f9d8f5fa470767047bc3
Fabio Riva <f.riva@sotefin.ch>
2025-11-21 10:16:55 +0100

    install date-fns library

--- app/hooks/use-mobile.ts
+++ app/hooks/use-mobile.ts
@@ -1,19 +1,42 @@
-import * as React from "react"
+import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+export function useIsMobile(breakpoint: number = 76) {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined
+  );
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
 
-  return !!isMobile
+  return !!isMobile;
 }
+
+// // hooks/use-mobile.ts
+// import { useEffect, useState } from "react";
+
+// // Custom hook to detect if the screen is mobile-sized
+// export function useIsMobile(breakpoint: number = 768) {
+//   const [isMobile, setIsMobile] = useState<boolean>(
+//     typeof window !== "undefined" ? window.innerWidth < breakpoint : false
+//   );
+
+//   useEffect(() => {
+//     function handleResize() {
+//       setIsMobile(window.innerWidth < breakpoint);
+//     }
+
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, [breakpoint]);
+
+//   return isMobile;
+// }
--- app/routes/aps/history.tsx
+++ app/routes/aps/history.tsx
@@ -1,15 +1,29 @@
+import { format, endOfDay, startOfDay, subDays } from "date-fns";
+import { useIsMobile } from "~/hooks/use-mobile";
+import { HistoryList } from "~/components/history-list";
 import fetcher from "~/lib/fetch.server";
 
 import type { Route } from "./+types/history";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const url = `${import.meta.env.VITE_BACKEND_URL}/${params?.aps}/dashboard`;
+  const from = format(
+    subDays(startOfDay(new Date()), 1),
+    "yyyy-MM-dd HH:mm:ss"
+  );
+  const to = format(endOfDay(new Date()), "yyyy-MM-dd HH:mm:ss");
+  const filter = "a";
+  const query = `system=0&dateFrom=${from}&dateTo=${to}&filter=${filter}&device=0&number=0`;
+  const url = `${import.meta.env.VITE_BACKEND_URL}/${params?.aps}/history?${query}`;
   const data = await fetcher(url);
   return { data };
 }
 
 export default function History({ loaderData, params }: Route.ComponentProps) {
-  // console.log(loaderData?.data);
+  console.log(loaderData?.data);
   if (!loaderData?.data) return <Error />;
-  return <h1>history</h1>;
+
+  const { count, query } = loaderData?.data;
+  console.log(query);
+  const isMobile = useIsMobile();
+  return <div>{isMobile ? <h1>mobile</h1> : <h1>desktop</h1>}</div>;
 }
