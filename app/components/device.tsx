eaa89cfa96f774453140ca0d4b2d57363084975f
Fabio Riva <f.riva@sotefin.ch>
2025-12-05 10:05:23 +0100

    added digital i/o translations

--- app/components/device.tsx
+++ app/components/device.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { DeviceView } from "~/components/device-view";
+import { DigitalIoInfo } from "~/components/digital-io-info";
 import { getDeviceInfo } from "~/lib/translation";
 import { useTranslation } from "react-i18next";
 
@@ -36,64 +37,70 @@ export function Device({ device, enhanced }: DeviceProps) {
         </CardDescription>
         <CardAction className="flex gap-1.5">
           {device.operation !== 0 && <Loader className="animate-spin" />}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            // fill={LA.status ? "#fecaca" : "none"}
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={
-              LA.status
-                ? "fill-alert/10 dark:fill-alert/20 text-alert"
-                : "fill-none text-slate-300"
-            }
-          >
-            <circle cx="12" cy="12" r="10" />
-            <line x1="12" x2="12" y1="8" y2="12" />
-            <line x1="12" x2="12.01" y1="16" y2="16" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            // fill={LC.status ? "#fed7aa" : "none"}
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={
-              LC.status
-                ? "fill-warning/10 dark:fill-warning/20 text-warning"
-                : "fill-none text-slate-300"
-            }
-          >
-            <circle cx="12" cy="12" r="10" />
-            <circle cx="12" cy="12" r="1" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            // fill={LS.status ? "fill-green-500" : "none"}
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={
-              LS.status
-                ? "fill-ready/10 dark:fill-ready/20 text-ready"
-                : "fill-none text-slate-300"
-            }
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="m9 12 2 2 4-4" />
-          </svg>
+          <DigitalIoInfo io={LA}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              // fill={LA.status ? "#fecaca" : "none"}
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={
+                LA.status
+                  ? "fill-alert/10 dark:fill-alert/20 text-alert"
+                  : "fill-none text-slate-300"
+              }
+            >
+              <circle cx="12" cy="12" r="10" />
+              <line x1="12" x2="12" y1="8" y2="12" />
+              <line x1="12" x2="12.01" y1="16" y2="16" />
+            </svg>
+          </DigitalIoInfo>
+          <DigitalIoInfo io={LC}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              // fill={LC.status ? "#fed7aa" : "none"}
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={
+                LC.status
+                  ? "fill-warning/10 dark:fill-warning/20 text-warning"
+                  : "fill-none text-slate-300"
+              }
+            >
+              <circle cx="12" cy="12" r="10" />
+              <circle cx="12" cy="12" r="1" />
+            </svg>
+          </DigitalIoInfo>
+          <DigitalIoInfo io={LS}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              // fill={LS.status ? "fill-green-500" : "none"}
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className={
+                LS.status
+                  ? "fill-ready/10 dark:fill-ready/20 text-ready"
+                  : "fill-none text-slate-300"
+              }
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="m9 12 2 2 4-4" />
+            </svg>
+          </DigitalIoInfo>
         </CardAction>
       </CardHeader>
       <CardContent className="flex flex-col gap-3 px-4">
---
+++ app/components/digital-io-info.tsx
@@ -0,0 +1,88 @@
+import { useTranslation } from "react-i18next";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+
+interface DeviceInfoProps {
+  output: any;
+}
+
+function get(label) {
+  // console.log(label);
+  switch (label) {
+    case "AD1":
+    case "AD2":
+    case "AD3":
+    case "AF7":
+    case "AF8":
+    case "DB32":
+    case "FE12":
+    case "K12":
+    case "L1":
+    case "L2":
+    case "L3":
+    case "L4":
+    case "L5":
+    case "RS12":
+    case "RS3":
+    case "RT12":
+    case "S1":
+    case "S2":
+    case "T10":
+    case "T2":
+      return { key: label, query: {} };
+
+    default:
+      if (label) {
+        let array = label.split("");
+        const dash = array[array.length - 2];
+        if (dash === "-") {
+          const last = parseInt(array[array.length - 3]);
+          if (!isNaN(last)) {
+            return {
+              key: array.slice(0, -3).join("").concat("x"),
+              query: { nr: last },
+            };
+          } else {
+            return { key: label.slice(0, -2), query: {} };
+          }
+        }
+        const last = parseInt(array[array.length - 1]);
+        if (!isNaN(last)) {
+          return {
+            key: array.slice(0, -1).join("").concat("x"),
+            query: { nr: last },
+          };
+        } else {
+          return { key: label, query: {} };
+        }
+      } else {
+        return { key: "label", query: {} };
+      }
+  }
+}
+
+export function DigitalIoInfo({
+  io,
+  children,
+}: PropsWithChildren<DeviceInfoProps>) {
+  const { t } = useTranslation();
+  const { key, query } = get(io?.label);
+  return (
+    <Tooltip>
+      <TooltipTrigger>{children}</TooltipTrigger>
+      <TooltipContent>
+        <ul className="font-semiboldtext-sm">
+          <li>{io?.addr}</li>
+          <li>{io?.label}</li>
+          <li className={io?.status ? "text-ready" : "text-muted-foreground"}>
+            {io?.status ? "TRUE" : "FALSE"}
+          </li>
+          <li>{io.label ? key && t("aps.io." + key, query) : ""}</li>
+        </ul>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
