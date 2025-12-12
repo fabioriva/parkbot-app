74f2a9c4e456923cc77841b596ba800c22526138
Fabio Riva <f.riva@sotefin.ch>
2025-12-12 08:58:48 +0100

    better responsive design  using tailwind display utility instead of useIsMobile hook

--- app/routes/aps/history.tsx
+++ app/routes/aps/history.tsx
@@ -1,7 +1,7 @@
 import { format, endOfDay, startOfDay, subDays } from "date-fns";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useIsMobile } from "~/hooks/use-mobile";
+// import { useIsMobile } from "~/hooks/use-mobile";
 import { HistoryList } from "~/components/history-list";
 import { HistoryQuery } from "~/components/history-query";
 import { HistoryTable } from "~/components/history-table";
@@ -25,11 +25,11 @@ export async function loader({ params }: Route.LoaderArgs) {
 export default function History({ loaderData, params }: Route.ComponentProps) {
   // console.log(loaderData?.data);
   if (!loaderData?.data) return <Error />;
+  // const isMobile = useIsMobile();
+  const { t } = useTranslation();
 
   const [history, setHistory] = useState(loaderData?.data);
   const { count, dateFrom, dateTo, query } = history;
-  const isMobile = useIsMobile();
-  const { t } = useTranslation();
 
   const handleQuery = async ({ from, to }) => {
     const strFrom = format(startOfDay(from), "yyyy-MM-dd HH:mm:ss");
@@ -44,7 +44,34 @@ export default function History({ loaderData, params }: Route.ComponentProps) {
   };
   return (
     <React.Fragment>
-      {isMobile ? (
+      {
+        <React.Fragment>
+          <div className="block lg:hidden">
+            <div className="flex flex-col gap-3">
+              <h1 className="text-lg">{t("aps.history.title")}</h1>
+              <HistoryQuery
+                from={dateFrom}
+                to={dateTo}
+                handleQuery={handleQuery}
+              />
+            </div>
+            <HistoryList history={query} />
+          </div>
+
+          <div className="hidden lg:block">
+            <div className="flex items-center">
+              <h1 className="grow text-lg">{t("aps.history.title")}</h1>
+              <HistoryQuery
+                from={dateFrom}
+                to={dateTo}
+                handleQuery={handleQuery}
+              />
+            </div>
+            <HistoryTable history={history} />
+          </div>
+        </React.Fragment>
+      }
+      {/* {isMobile ? (
         <React.Fragment>
           <div className="flex flex-col gap-3">
             <h1 className="text-lg">{t("aps.history.title")}</h1>
@@ -68,7 +95,7 @@ export default function History({ loaderData, params }: Route.ComponentProps) {
           </div>
           <HistoryTable history={history} />
         </React.Fragment>
-      )}
+      )} */}
     </React.Fragment>
   );
 }
