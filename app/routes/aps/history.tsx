db378bd3cff831b3a9f562898d033470af3d6f88
Fabio Riva <f.riva@sotefin.ch>
2026-03-17 11:31:10 +0100

    simplify history page responsive layout

--- app/routes/aps/history.tsx
+++ app/routes/aps/history.tsx
@@ -51,23 +51,12 @@ export default function History({ loaderData, params }: Route.ComponentProps) {
   };
   return (
     <>
-      <div className="block md:hidden">
+      <div className="block xl:hidden">
         <div className="flex flex-col gap-3 mb-3">
           <h1 className="text-lg">{t("history.title")}</h1>
           <HistoryQuery from={dateFrom} to={dateTo} handleQuery={handleQuery} />
         </div>
         <HistoryList history={query} />
-      </div>
-      <div className="hidden md:block xl:hidden">
-        <Item variant="outline">
-          <ItemContent>
-            <ItemTitle>{t("history.title")}</ItemTitle>
-            <ItemDescription>
-              {t("history.description", { from: dateFrom, to: dateTo, count })}
-            </ItemDescription>
-          <HistoryList history={query} />
-          </ItemContent>
-        </Item>
       </div>
       <div className="hidden xl:block">
         <Item variant="outline">
