f0d3ea3c14f1938f605b251f69feaf754cda81d6
Fabio Riva <f.riva@sotefin.ch>
2026-03-26 15:17:03 +0100

    fix: show active alarms badge if active

--- app/components/alarm-info.tsx
+++ app/components/alarm-info.tsx
@@ -12,7 +12,7 @@ interface AlarmInfoProps {
 export function AlarmInfo({ active }: AlarmInfoProps) {
   return (
     <>
-      {!active > 0 && (
+      {active > 0 && (
         <Tooltip>
           <TooltipTrigger asChild>
             <Badge className="bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-300">
