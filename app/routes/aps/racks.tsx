58a8cf0b8dc5d5aae0473509bc77913e3ec6b377
Fabio Riva <f.riva@sotefin.ch>
2026-03-27 10:27:32 +0100

    racks route initial commit

--- app/routes/aps/racks.tsx
+++ app/routes/aps/racks.tsx
@@ -1,6 +1,17 @@
+import { CircleFadingArrowUpIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useFetcher } from "react-router";
+import { Button } from "~/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemMedia,
+  ItemTitle,
+} from "~/components/ui/item";
 import { getCookie } from "~/lib/cookie.server";
 import fetcher from "~/lib/fetch.server";
 import type { Route } from "./+types/racks";
@@ -27,10 +38,28 @@ export default function Dashboard({
     );
   const [data, setData] = useState(loaderData);
 
+  // return (
+  //   <div>
+  //     <h2>Data</h2>
+  //     <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
+  //   </div>
+  // );
+
   return (
-    <div>
-      <h2>Data</h2>
-      <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <ItemGroup className="w-full lg:max-w-sm gap-3">
+      {data.map((item) => (
+        <Item variant="outline" key={item.deviceNr}>
+          <ItemContent>
+            <ItemTitle>{item.deviceName} I/Os</ItemTitle>
+            <ItemDescription>{item.type}</ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <Button variant="outline" size="icon">
+              <CircleFadingArrowUpIcon />
+            </Button>
+          </ItemActions>
+        </Item>
+      ))}
+    </ItemGroup>
   );
 }
