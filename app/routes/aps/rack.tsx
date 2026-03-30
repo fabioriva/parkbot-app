973a15a23c92e8db7d1ff3964105d7b89f6faef3
Fabio Riva <f.riva@sotefin.ch>
2026-03-30 10:12:26 +0200

    new rack design

--- app/routes/aps/rack.tsx
+++ app/routes/aps/rack.tsx
@@ -21,12 +21,39 @@ export default function Rack({ loaderData, params }: Route.ComponentProps) {
       </h1>
     );
   const url = `${import.meta.env.VITE_WEBSOCK_URL}/${params.aps}/racks/${params.nr}`;
-  const { data } = useData(url, { initialData: loaderData?.data });
+  const { data } = useData(url, { initialData: loaderData });
+
+  // return (
+  //   <div>
+  //     <h2>Data</h2>
+  //     <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
+  //   </div>
+  // );
 
   return (
-    <div>
-      <h2>Data</h2>
-      <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
+    <div className="flex overflow-scroll gap-0.5 py-3">
+      {data.cards.map((card) => (
+        <div
+          className="bg-slate-400 flex flex-col gap-0.5 border p-1 relative text-xs text-white"
+          key={card.nr}
+        >
+          <p className="text-[0.625rem]">{card.type}</p>
+          {card.bytes.map((byte) => (
+            <div className="flex flex-col gap-0.5 text-black" key={byte.label}>
+              <p className="mt-1.5">Byte {byte.label.substring(0, 1) + "B" + byte.label.substring(1)}</p>
+              {byte.bits.map((bit) => (
+                <div className="bg-white flex border" key={bit.addr}>
+                  <span className="w-12">{bit.addr}</span>
+                  <span className="w-16">{bit.label}</span>
+                  <span
+                    className={`text-center w-3 ${bit.status ? "bg-green-500" : "bg-gray-300"}`}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
