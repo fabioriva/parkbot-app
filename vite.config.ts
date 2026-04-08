6384d4c397bed6e256abd89b96d792f965142c14
Fabio Riva <f.riva@sotefin.ch>
2026-04-08 09:33:06 +0200

    react router updated to release 7.14.0 added support for Vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   optimizeDeps: {
     exclude: ["@node-rs/argon2", "@node-rs/bcrypt"],
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
