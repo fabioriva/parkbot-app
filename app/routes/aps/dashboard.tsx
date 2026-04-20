c154225814d66ab7f329e4bb0d0a178b901c842f
Fabio Riva <f.riva@sotefin.ch>
2026-04-20 09:06:43 +0200

    modified it translations for better ui

--- app/routes/aps/dashboard.tsx
+++ app/routes/aps/dashboard.tsx
@@ -83,7 +83,7 @@ export default function Dashboard({
             </CardAction>
           </CardHeader>
           <CardContent>
-            <HistoryList query={activity.documents} media={true} />
+            <HistoryList query={activity.documents} />
           </CardContent>
         </Card>
         <Operations
