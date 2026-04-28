ecc659b4ea421a36f3cc0ddc9efb4ada9f9b4235
Fabio Riva <f.riva@sotefin.ch>
2026-04-28 09:30:59 +0200

    error boundary page styling

--- app/root.tsx
+++ app/root.tsx
@@ -154,11 +154,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main>
+    <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre>
+        <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
         </pre>
       )}
