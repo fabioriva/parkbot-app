6972b1cf37b92c41f9df3ae956a14ebea10c8ac7
Fabio Riva <f.riva@sotefin.ch>
2026-05-13 14:28:21 +0200

    customize user table + session response using customSession plugin

--- app/lib/auth.server.ts
+++ app/lib/auth.server.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth/minimal";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { customSession, haveIBeenPwned, twoFactor } from "better-auth/plugins";
+import { customSession, haveIBeenPwned, twoFactor } from "better-auth/plugins";
 import { db, findAps } from "./db.server";
 // import { sendEmail } from "./email.server";
 
@@ -12,5 +12,35 @@ export const auth = betterAuth({
   database: mongodbAdapter(db),
   emailAndPassword: {
     enabled: true,
+  },
+  // ...
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const aps = await findAps(user.aps);
+      return {
+        aps,
+        user,
+        session,
+      };
+    }),
+    haveIBeenPwned(),
+    twoFactor({
+      skipVerificationOnEnable: false,
+    }),
+  ],
+  user: {
+    additionalFields: {
+      aps: {
+        type: "string",
+        input: true,
+        required: false, // set to false because selected after signin/up
+      },
+      role: {
+        type: ["admin", "service", "valet"],
+        defaultValue: "service",
+        input: true,
+        required: true,
+      },
+    },
   },
 });
