3d87becf2a56d08d4cda839ef66e20d98e20ff3b
Fabio Riva <f.riva@sotefin.ch>
2026-05-20 15:11:43 +0200

    change password route first commit

--- app/routes.ts
+++ app/routes.ts
@@ -31,7 +31,7 @@ export default [
       //     route(":aps/rack/:nr", "routes/aps/rack.tsx"),
       //     route(":aps/tags", "routes/aps/tags.tsx"),
       //     route(":aps/user/2fa", "routes/user/2fa.tsx"),
-      //     route(":aps/user/password", "routes/user/password.tsx"),
+      route(":aps/user/password", "routes/user/password.tsx"),
     ]),
   ]),
   route("action/set-theme", "./routes/action.set-theme.ts"),
