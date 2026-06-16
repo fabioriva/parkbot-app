8ce466a380806104c8b12b0dcea1c4426828c804
Fabio Riva <f.riva@sotefin.ch>
2026-06-16 15:19:15 +0200

    x padding decreased

--- app/routes/auth/layout.tsx
+++ app/routes/auth/layout.tsx
@@ -3,8 +3,8 @@ import { AuroraText } from "~/components/aurora-text";
 
 export default function Auth() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 px-3">
+      <div className="flex w-full max-w-sm flex-col gap-3">
         <a href="/" className="flex items-center gap-2 self-center font-medium">
           <h1 className="text-3xl md:text-4xl font-extrabold tracking-tighter">
             <AuroraText>Parkbot</AuroraText> auth service
