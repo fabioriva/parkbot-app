9865512bbf7da620d4860dd204f4a383e8148e6a
Fabio Riva <parkbot@parkbot-app.sotefinservice.com>
2026-07-28 11:04:10 +0200

    ecosystem file migliorato per uitilizzare node v24 come interpreter

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,17 @@ module.exports = {
     {
       name: "parkbot-app",
       script: "pm2",
-      cwd: "./",
-      interpreter: "npm",
+      cwd: "/home/parkbot/github/parkbot-app",
+      interpreter: "/home/parkbot/.nvm/versions/node/v24.18.0/bin/npm",
       interpreter_args: "start",
-      max_memory_restart: '128M',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      out_file: '/dev/null'
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      },
+      max_memory_restart: "128M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      out_file: "/dev/null",
+      watch: false
     }
   ]
 }
